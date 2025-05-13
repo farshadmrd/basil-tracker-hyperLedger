7f7939329546ccc709568e0a60bf9c10aaef9791
farshadmrd <fa.moradi2000@gmail.com>
2025-05-13 13:41:29 +0200

    change name to "BasilContract"

--- chaincode-template/src/main/java/org/hyperledger/fabric/samples/BasilContract.java
+++ chaincode-template/src/main/java/org/hyperledger/fabric/samples/BasilContract.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @Contract(
-        name = "basic",
+        name = "BasilContract",
         info = @Info(
                 title = "My Smart contract",
                 description = "The hyperlegendary asset transfer",
